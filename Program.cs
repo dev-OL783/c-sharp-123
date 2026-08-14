4a3936c089176b5bbd73b46a4de5066d60d6498d
dev-OL783 <dev_783@tutamail.com>
2026-08-14 23:17:05 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace A1Q3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Keep this line intact 
+        Console.WriteLine("===========================");
+
+        // prompts the user to enter a cricket score result
+        Console.WriteLine("Please enter the over scorecard:");
+        // converts results to string
+        string scorecard = Console.ReadLine();
+
+        // only accept numbers between 1 and 6 characters [1, 6] 
+        // for runs, numbers can be displayed between 1 and 6 representing the number of runs a ball scores 
+        // W for wickets, symbolizing a successful hit from the baller
+        // . representing no runs were successfully made
+        // any other input/character should make the scorecard invalid and cause a invalid message
+
+
+
+
+        // Keep this line intact 
+        Console.WriteLine("===========================");
+    }
+}  

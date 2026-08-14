23b05515ea8e4043e3d85e7144638921d90198ea
dev-OL783 <dev_783@tutamail.com>
2026-08-14 23:05:27 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace A1Q3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Keep this line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here.
+        Console.WriteLine("Please enter the over scorecard:");
+        string scorecard = Console.ReadLine();
+        
+
+        // Keep this line intact 
+        Console.WriteLine("===========================");
+    }
+}  
